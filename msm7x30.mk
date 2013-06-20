c2bd74747949637470c4912fb1a90b6e15bc4680
Michael Bestas <mikeioannina@gmail.com>
2013-06-20 19:47:02 +0300

    Proprietary side needs to exist

--- msm7x30.mk
+++ msm7x30.mk
@@ -156,4 +156,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.strictmode.visual=0 \
     persist.sys.strictmode.disable=1
 
-$(call inherit-product-if-exists, vendor/semc/msm7x30-common/msm7x30-common-vendor.mk)
+# proprietary side of the board
+$(call inherit-product, vendor/semc/msm7x30-common/msm7x30-common-vendor.mk)
