b91b2ef8cddb9f5432d707592dc14bbb1a357903
Michael Bestas <mikeioannina@gmail.com>
2013-08-28 16:37:46 +0300

    camera: detect urushi torch with a smarter way
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ia06ef56ca693271bb118b788ec1b6167d791f0ef

--- camera/Android.mk
+++ camera/Android.mk
@@ -7,6 +7,10 @@ LOCAL_SRC_FILES := \
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
+ifeq ($(TARGET_DEVICE),urushi)
+    LOCAL_CFLAGS += -DUSES_AS3676_TORCH
+endif
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.semc
 
