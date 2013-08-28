2358eb7e83923e9633bfcf78fcbf65d50ac43aa2
Michael Bestas <mikeioannina@gmail.com>
2013-08-28 15:51:36 +0300

    liblights: include device specific header
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Iceae8a2ff01fe6e0f3e01403e07f7579cea34de4

--- liblights/lights.c
+++ liblights/lights.c
@@ -32,23 +32,12 @@
 #include <sys/types.h>
 
 #include <hardware/lights.h>
+#include "semc_lights.h"
 
 char const*const LCD_BACKLIGHT_FILE = "/sys/class/leds/lcd-backlight/brightness";
 char const*const RED_LED_FILE       = "/sys/class/leds/red/brightness";
 char const*const GREEN_LED_FILE     = "/sys/class/leds/green/brightness";
 char const*const BLUE_LED_FILE      = "/sys/class/leds/blue/brightness";
-
-char const*const BUTTON_BACKLIGHT_FILE[] = {
-  "/sys/class/leds/button-backlight-rgb1/brightness",
-  "/sys/class/leds/button-backlight-rgb2/brightness"
-};
-
-char const*const KEYBOARD_BACKLIGHT_FILE[] = {
-  "/sys/class/leds/keyboard-backlight-rgb1/brightness",
-  "/sys/class/leds/keyboard-backlight-rgb2/brightness",
-  "/sys/class/leds/keyboard-backlight-rgb3/brightness",
-  "/sys/class/leds/keyboard-backlight-rgb4/brightness"
-};
 
 char const*const LED_FILE_TRIGGER[]  = {
   "/sys/class/leds/red/use_pattern",
