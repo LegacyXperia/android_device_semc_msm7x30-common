1e67bbe93b8576d83cb2cbe5c45d22c2e802b07c
Michael Bestas <mikeioannina@gmail.com>
2013-11-20 05:27:49 +0200

    releasetools: proper device assert workaround
    Update edify_generator from upstream

--- releasetools/edify_generator.py
+++ releasetools/edify_generator.py
@@ -95,7 +95,9 @@ class EdifyGenerator(object):
     cmd = ('assert(' +
            ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
                          % (i, i) for i in device.split(",")]) +
-           ');')
+           ' || abort("This package is for \\"%s\\" devices; '
+           'this is a \\"" + getprop("ro.product.device") + "\\".");'
+           ');') % device
     self.script.append(self._WordWrap(cmd))
 
   def AssertSomeBootloader(self, *bootloaders):
