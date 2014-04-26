e1de3b1f29ec8450dc21276a7afd6d669f74f01d
Michael Bestas <mikeioannina@gmail.com>
2014-04-26 03:51:40 +0300

    msm7x30: Update recovery_keys.c
    The default key mapping file in recovery is being updated to strip out
    display_toggle and some ifs (see http://review.cyanogenmod.org/62724 ).
    The custom key file here needs to make similar changes.

--- recovery/recovery_keys.c
+++ recovery/recovery_keys.c
@@ -4,13 +4,6 @@
 #include "common.h"
 #include "extendedcommands.h"
 
-
-int device_toggle_display(volatile char* key_pressed, int key_code) {
-    // hold power and press volume-up
-    return key_pressed[KEY_POWER] && key_code == KEY_VOLUMEUP;
-}
-
-
 int device_handle_key(int key_code, int visible) {
     if (visible) {
         switch (key_code) {
@@ -26,10 +19,6 @@ int device_handle_key(int key_code, int visible) {
                 return HIGHLIGHT_UP;
 
             case KEY_POWER:
-                if (ui_get_showing_back_button()) {
-                    return SELECT_ITEM;
-                }
-                break;
             case KEY_HOME:
             case KEY_LEFTBRACE:
             case KEY_ENTER:
@@ -37,15 +26,15 @@ int device_handle_key(int key_code, int visible) {
             case KEY_CAMERA:
             case KEY_F21:
             case KEY_SEND:
+            case KEY_HOMEPAGE:
                 return SELECT_ITEM;
             case KEY_END:
             case KEY_BACKSPACE:
             case KEY_SEARCH:
-                if (ui_get_showing_back_button()) {
-                    return SELECT_ITEM;
-                }
             case KEY_BACK:
-                return GO_BACK;
+                if (!ui_root_menu) {
+                    return GO_BACK;
+                }
         }
     }
 
