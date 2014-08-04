e74942341d92a0ab254b3705ff29b36cc95e4645
Michael Bestas <mikeioannina@gmail.com>
2014-08-04 19:14:27 +0300

    msm7x30: Remove obsolete props

--- msm7x30.mk
+++ msm7x30.mk
@@ -163,8 +163,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.hw=1 \
-    debug.egl.hw=1 \
     debug.composition.type=dyn \
     persist.hwc.mdpcomp.enable=false \
     debug.mdpcomp.maxlayer=3 \
