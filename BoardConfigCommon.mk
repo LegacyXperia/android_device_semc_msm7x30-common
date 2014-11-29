70ddfb0875e2166b921005d504dc45de92c25929
Michael Bestas <mikeioannina@gmail.com>
2014-11-29 17:27:36 +0200

    Revert "msm7x30: Use cortex-a8 cpu variant"
    * Use scorpion variant again
    
    This reverts commit ca3f5964a69e20b7e5e03e4ffc68fe33fba99587.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,7 +31,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a8
+TARGET_CPU_VARIANT := scorpion
 
 # Qualcomm Hardware
 BOARD_USES_QCOM_HARDWARE := true
