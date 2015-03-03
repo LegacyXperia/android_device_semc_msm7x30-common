2492e5e5105b09f7dd9113ff1ff41d509c9ed982
Michael Bestas <mikeioannina@gmail.com>
2015-03-04 00:20:50 +0200

    msm7x30-common: Fix adaptive backlight
    * I still don't like this code at all but it should work properly now
    
    Change-Id: Ieba10672edfe4f26d2f89a5c6c5e02823bce7935

--- cmhw/org/cyanogenmod/hardware/AdaptiveBacklight.java
+++ cmhw/org/cyanogenmod/hardware/AdaptiveBacklight.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 The CyanogenMod Project
+ * Copyright (C) 2013-2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,15 +37,26 @@ public class AdaptiveBacklight {
         File f3 = new File(SII_SYSFS);
         File f4 = new File(SONY_SYSFS);
 
-        if (f1.exists() || f2.exists() || f3.exists() || f4.exists()) {
-            return true;
-        } else {
-            return false;
-        }
+        return (f1.exists() || f2.exists() || f3.exists() || f4.exists());
     }
 
     public static boolean isEnabled() {
-        return isSupported();
+        File f1 = new File(AUO_SYSFS);
+        File f2 = new File(HITACHI_SYSFS);
+        File f3 = new File(SII_SYSFS);
+        File f4 = new File(SONY_SYSFS);
+
+        if (f1.exists()) {
+            return Boolean.parseBoolean(FileUtils.readOneLine(AUO_SYSFS));
+        } else if (f2.exists()) {
+            return Boolean.parseBoolean(FileUtils.readOneLine(HITACHI_SYSFS));
+        } else if (f3.exists()) {
+            return Boolean.parseBoolean(FileUtils.readOneLine(SII_SYSFS));
+        } else if (f4.exists()) {
+            return Boolean.parseBoolean(FileUtils.readOneLine(SONY_SYSFS));
+        } else {
+            return false;
+        }
     }
 
     public static boolean setEnabled(boolean status) {
