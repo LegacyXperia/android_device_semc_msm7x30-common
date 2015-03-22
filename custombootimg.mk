a70389254c82e9d885b16798c35177ed9e0ce4e2
Mike Gapinski <mikegapinski@gmail.com>
2015-03-22 19:39:33 +0100

    Fix OSX compilation: zcat/gzcat works in linux only
    Change-Id: Iacbb3b82999bf2cdf48e2aa8b6c10d1786ef90ee

--- custombootimg.mk
+++ custombootimg.mk
@@ -16,7 +16,7 @@ LOCAL_PATH := $(call my-dir)
 
 uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
 $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
-	zcat $< > $@
+	gunzip -c $< > $@
 
 INITSH := $(LOCAL_PATH)/combinedroot/init.sh
 
