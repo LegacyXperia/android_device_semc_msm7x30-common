50055a81c04df684d60eb6f27a175437161cd791
AdrianDC <radian.dc@gmail.com>
2016-01-07 10:15:51 +0100

    msm7x30: Implement a source-built SEMC init
     * Upgrade from the init.sh script to a C++ executable
     * Provide generic functions & device specific sources
     * Update from deprecated busybox to toybox features
    
    Change-Id: I8235a3d66912a4051b653a6a312965dcaddeb9d2
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

---
+++ init_semc/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    init_exec.cpp \
+    init_files.cpp \
+    init_main.cpp \
+    init_board_semc.cpp
+
+LOCAL_CPPFLAGS := \
+    -Wall \
+    -Wextra \
+    -Werror
+
+LOCAL_MODULE := init_semc
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
+LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := \
+    libc \
+    libcutils
+
+LOCAL_CLANG := true
+LOCAL_SANITIZE := integer
+
+include $(BUILD_EXECUTABLE)
