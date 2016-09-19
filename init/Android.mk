CyanogenMod/android_device_sony_common

init: Use a more conventional ramdisk layout
The current ramdisk layout for sony devices does not obey to the
convention where all ramdisk files reside directly within the root of
the ramdisk (e.g., /init). Instead, the actual content of the Android
ramdisk is packed in the file '/sbin/ramdisk.cpio' that is extracted
when performing an Android boot. This is done to also pack a recovery
ramdisk and allow booting from it.

Unfortunately, due to Google's change in the update mechanism, many
tools (e.g., SuperSU and Xposed) now work on the ramdisk instead of the
system partition. This conflicts with the ramdisk layout of the sony
devices using the init_sony mechanism. Since this layout does not affect
too many users, the required changes in the tools will probably never go
upstream and would require patching every single release.

This commit therefore changes the init_sony mechanism to use a
conventional layout of the ramdisk, while still providing the ability to
boot from a packed recovery. Also, booting from the FOTA partition is
still possible. If the user decides to boot into recovery, the ramdisk
files belonging to the Android ramdisk are removed such that a clean
state is achieved before unpacking the recovery ramdisk.

For this change to operate correctly, an update to the device
custombootimg.mk is required, see the commit uploaded separately.

In particular this commit introduces the following changes:

 - Introduce a files "starting / ending with" deleter in init_files.cpp
 - Remove the Android ramdisk files before extracting the recovery,
   added to a new init_ramdisk.cpp file listing the ramdisk files

Change-Id: I816b5168d0de9c6987cc3b36fbd659fb2404f94b
Signed-off-by: Adrian DC <radian.dc@gmail.com>

init/Android.mk
@@ -23,6 +23,7 @@ LOCAL_SRC_FILES := \
     init_files.cpp \
     init_io.cpp \
     init_main.cpp \
+    init_ramdisk.cpp \
     ../extract_ramdisk/extract_ramdisk.cpp
 
 LOCAL_C_INCLUDES := \

LegacyXperia/android_device_semc_msm7x30-common

init/Android.mk
@@ -22,7 +22,8 @@ LOCAL_SRC_FILES := \
     init_exec.cpp \
     init_files.cpp \
     init_io.cpp \
-    init_main.cpp
+    init_main.cpp \
+    init_ramdisk.cpp
 
 LOCAL_CPPFLAGS := \
     -Wall \