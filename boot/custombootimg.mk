52763b54034fd840cc88106d2320166428478b8d
SteadyQuad <SteadyQuad@gmail.com>
2016-10-03 17:18:31 +0200

    msm7x30: pack ramdisk with lzma, set size limit for boot.img
    With CM-14.0 the semc boot.img apparently crossed a size limit and
    caused problems: failure to boot.
    
    Packing the combined ramdisk with lzma makes boot.img smaller.
    In addition a size check was added to check that boot.img stays
    below 0x00900000 (9437184 bytes). This limit is an approximation.
    
    Change-Id: Ie25c116ce981bed2b39a2121d6e2264e1c030456

--- boot/custombootimg.mk
+++ boot/custombootimg.mk
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 LOCAL_PATH := $(call my-dir)
+LZMA_BIN := $(shell which lzma)
 
 uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
 $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
@@ -69,9 +70,10 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel \
 	$(hide) ln -s sbin/init_semc $(PRODUCT_OUT)/combinedroot/init
 
 	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/combinedroot/ > $(PRODUCT_OUT)/combinedroot.cpio
-	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | $(MINIGZIP) -9 > $(PRODUCT_OUT)/combinedroot.fs
+	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | $(LZMA_BIN) -9 > $(PRODUCT_OUT)/combinedroot.fs
 
 	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel --ramdisk $(PRODUCT_OUT)/combinedroot.fs --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) -o $(INSTALLED_BOOTIMAGE_TARGET)
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE))
 	@echo "Made boot image: $@"
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
